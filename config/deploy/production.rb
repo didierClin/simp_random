4fcec5511e7ea034f9363f75a3430633e52828ee
didierClin <didier.c62@numericable.fr>
2016-02-19 21:37:50 +0100

    prepa cap deploy migrate

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '51.255.201.63', user: 'deploy', roles: %w{web app}
+server '51.255.201.63', user: 'deploy', roles: %w{web app db}
